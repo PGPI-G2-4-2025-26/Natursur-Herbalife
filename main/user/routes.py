c7354b65bf59d4dea660c8044696ea4795b522fb
davchacum <davchacum@alum.us.es>
2025-11-18 20:46:28 +0100

    feat: Añadido login de usuarios

--- main/user/routes.py
+++ main/user/routes.py
@@ -1,13 +1,16 @@
-from django.urls import path, include
+from django.urls import path
 from django.conf import settings
 from main.user import views
+from main.user import forms as user_forms
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path('cuentas/', include('django.contrib.auth.urls')), 
-    path('registro/', views.registration, name='registration'), 
-    path('login/', views.login, name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html', authentication_form=user_forms.ClientLoginForm), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('registro/', views.registration, name='registration'),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/user/views.py
+++ main/user/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ClientCreationForm
 
 
 def registration(request):
-    form = ClientCreationForm()
+    if request.method == 'POST':
+        form = ClientCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = ClientCreationForm()
+
     return render(request, 'registration.html', {'form': form})
+
 
 def login(request):
     return render(request, 'login.html')
