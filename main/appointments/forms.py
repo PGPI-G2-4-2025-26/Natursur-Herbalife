c8ef8e38e10493ff7497d280c1210dd35fc22f50
Marco2026 <marherluj@alum.us.es>
2025-11-25 14:10:39 +0100

    feat: finished services frontend screen

--- main/appointments/forms.py
+++ main/appointments/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 
+
 class CreateAppointmentForm(forms.Form):
     name = forms.CharField(
         max_length=255,
+        label='Nombre',
         widget=forms.TextInput(attrs={
             'placeholder': 'Nombre de la sesión',
             'class': 'form-control',
@@ -10,6 +12,7 @@ class CreateAppointmentForm(forms.Form):
     )
     price = forms.DecimalField(
         max_digits=8,
+        label='Precio',
         decimal_places=2,
         widget=forms.NumberInput(attrs={
             'placeholder': 'precio',
@@ -17,6 +20,7 @@ class CreateAppointmentForm(forms.Form):
         })
     )
     duration = forms.IntegerField(
+        label='Duración',
         widget=forms.NumberInput(attrs={
             'placeholder': 'Duración en minutos',
             'class': 'form-control',
@@ -24,21 +28,25 @@ class CreateAppointmentForm(forms.Form):
     )
     description = forms.CharField(
         required=False,
+        label='Descripción',
         widget=forms.Textarea(attrs={
             'placeholder': 'Descripción',
             'class': 'form-control',
         })
     )
     premium = forms.BooleanField(
+        label='Premium',
         required=False,
         widget=forms.CheckboxInput(attrs={
             'class': 'form-check-input',
         })
     )
 
+
 class CreateDiscountForm(forms.Form):
     discount = forms.DecimalField(
         max_digits=5,
+        label='Precio',
         decimal_places=2,
         widget=forms.NumberInput(attrs={
             'placeholder': 'Precio del descuento',
@@ -47,7 +55,7 @@ class CreateDiscountForm(forms.Form):
     )
 
     current_end_date = forms.CharField(
-        label="Fecha fin de descuento actual",
+        label="Fecha de fin de descuento actual",
         required=False,
         widget=forms.TextInput(attrs={
             'class': 'form-control',
@@ -63,7 +71,7 @@ class CreateDiscountForm(forms.Form):
     )
 
     endDiscount = forms.DateTimeField(
-        label="Nueva fecha fin de descuento",
+        label="Nueva fecha de fin de descuento",
         required=False,
         widget=forms.DateTimeInput(attrs={
             'class': 'form-control',
