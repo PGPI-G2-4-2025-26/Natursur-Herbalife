bbbf4f7dc682e920d3e78adccd3b9a9305ec8cd5
Francisco Gago <frangago71@gmail.com>
2025-11-25 20:24:56 +0100

    feat: Pagina de perfil con edicion disponible

--- main/user/views.py
+++ main/user/views.py
@@ -1,5 +1,9 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from .forms import ClientCreationForm, ClientLoginForm
+
+from main.user.models import UserProfile
+from .forms import ClientCreationForm, ClientLoginForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth import logout, login as auth_login
 from django.conf import settings
 
@@ -41,5 +45,35 @@ def logout_view(request):
     return render(request, 'logout.html')
 
 
+@login_required
 def profile(request):
-    return render(request, 'profile.html')
+    if not hasattr(request.user, 'userprofile'):
+        UserProfile.objects.create(user=request.user)
+
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.userprofile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            profile_obj = p_form.save(commit=False)
+            
+            if request.POST.get('delete_photo') == 'true':
+                profile_obj.photo.delete(save=False)
+                
+                profile_obj.photo = None 
+            
+            profile_obj.save()
+            
+            messages.success(request, '¡Tu perfil ha sido actualizado!')
+            return redirect('profile')
+            
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.userprofile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'profile.html', context)
