3d1986f0575b18c5ba328758be2892a951a69f48
DavidBBSA <davbasbra@alum.us.es>
2025-11-26 19:05:46 +0100

    Configuración de despliegue modificada

--- natursur/settings.py
+++ natursur/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'default-dummy-key-for-local-testing-only')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['natursur-herbalife.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
