a97984513be0d173a7d688547ed4b8c97b514e9b
Marco Antonio <114426852+Marco2026@users.noreply.github.com>
2025-11-26 19:42:08 +0100

    Add collectstatic command to build script

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ pip install -r requirements.txt
 python manage.py migrate 
 python manage.py shell -c "from main.import_csv import cargar; print(cargar())" 
 python manage.py seed
+python manage.py collectstatic --noinput
