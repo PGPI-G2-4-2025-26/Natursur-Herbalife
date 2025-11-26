065b737e23f72707e04996bf8f19af8bea2a5bcf
Marco Antonio <114426852+Marco2026@users.noreply.github.com>
2025-11-26 20:40:22 +0100

    Update ALLOWED_HOSTS for deployment

--- natursur/settings.py
+++ natursur/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'default-dummy-key-for-local-testing-o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['natursur-herbalife.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['naturtest.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
