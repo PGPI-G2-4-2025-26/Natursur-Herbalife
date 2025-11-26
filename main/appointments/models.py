4c09ba179d02c3ab99d78a44745dfcc8c312a328
Marco Antonio <114426852+Marco2026@users.noreply.github.com>
2025-11-26 21:58:17 +0100

    Import datetime for appointment model

--- main/appointments/models.py
+++ main/appointments/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator
-
+from datetime.datetime import datetime
 
 class Appointment(models.Model):
     name = models.CharField(max_length=255, verbose_name="Name")
