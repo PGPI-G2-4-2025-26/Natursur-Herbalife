53dcd273f140e84b65eb6858e5130bfab74fc311
Marco Antonio <114426852+Marco2026@users.noreply.github.com>
2025-11-26 19:19:44 +0100

    Add build script for setup and seeding database

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash 
+set -o errexit 
+pip install -r requirements.txt 
+python manage.py migrate 
+python manage.py shell -c "from main.import_csv import cargar; print(cargar())" 
+python manage.py seed
